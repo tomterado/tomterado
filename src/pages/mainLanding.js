8eddc24e9317fc8996c579a67dc366eeed40143f
Tom <tom.terado@gmail.com>
2019-07-16 01:13:47 +1000

    Remove image

--- src/pages/mainLanding.js
+++ src/pages/mainLanding.js
@@ -56,9 +56,6 @@ class MainLanding extends React.Component {
       <div style={styles.container}>
         <div style={{ alignItems: "center" }}>
           <h1 style={styles.title}> tom terado </h1>
-          {/* <h1 style={styles.title}> front-end developer </h1>  */}
-          {/* <h3 style={styles.secondaryTitle}> product.</h3> */}
-          {/* <h3 style={styles.secondaryTitle}> front-end developer.</h3> */}
 
           <h3 style={styles.description}>
             {" "}
@@ -68,19 +65,7 @@ class MainLanding extends React.Component {
             <span style={styles.smallerTextStroke}> tech </span> and{" "}
             <span style={styles.smallerTextStroke}> art.</span>{" "}
           </h3>
-          {/* <hr style={styles.shorterLine2}/> */}
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            {/* <img align="middle" src='https://ih1.redbubble.net/image.69173097.0090/ap,550x550,12x12,1,transparent,t.u1.png' /> */}
-            {/* <img align="middle" src='https://cdn.dribbble.com/users/43342/videos/24356/comp_1.mp4' /> */}
-            <iframe
-              width="400"
-              height="300"
-              src="https://cdn.dribbble.com/users/2528235/screenshots/6595156/goodnight_3.gif"
-            >
-              {" "}
-            </iframe>
-            {/*<video width="400" height="300" src="https://cdn.dribbble.com/users/2528235/screenshots/6595156/goodnight_3.gif"/>*/}
-          </div>
+          <div style={{ display: "flex", justifyContent: "center" }} />
         </div>
       </div>
     )
